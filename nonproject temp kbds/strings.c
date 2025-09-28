361ceaf1a11fa7f96367f9d3525eac2e6076f525
Admin <admin@example.com>
2025-09-28 20:21:41 +0500

    + kbus to examples

---
+++ nonproject temp kbds/strings.c
@@ -0,0 +1,11 @@
+#define WKL_TEXT "US"
+#define WKL_LANG "0409"
+
+// Other possible matching entries:
+// 00000404: "Chinese (Traditional) - US"
+// 00000409: "US"
+// 00000804: "Chinese (Simplified) - US"
+// 00000c04: "Chinese (Traditional, Hong Kong S.A.R.) - US"
+// 00001004: "Chinese (Simplified, Singapore) - US"
+// 00001404: "Chinese (Traditional, Macao S.A.R.) - US"
+// 00010402: "Bulgarian (Latin)"
